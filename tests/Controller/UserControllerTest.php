af7fa73ac0cd58dc6758ca4895e952747d949f1b
Olivier-Preaux <olivier.prx@gmail.com>
2021-05-05 10:02:40 +0200

    feat: tests controllers

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\tests\Controller;
+
+use App\Entity\TennisMatch;
+use App\Entity\User;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\DomCrawler\Field\InputFormField;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testUserNew()
+    {   
+        $client = static::createClient(); 
+        $crawler = $client->request('GET', '/register');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $email= 'test@test.com';
+        $form = $crawler->selectButton('registration_form[save]')->form();
+        $form['registration_form[email]'] = $email; 
+        $form['registration_form[plainPassword][first]'] = 'test123';
+        $form['registration_form[plainPassword][second]'] = 'test123';
+        $form['registration_form[pseudo]'] = 'pseudotest';
+        $form['registration_form[sex]'] = 'Homme';
+        $form['registration_form[level]'] = 'Expert' ;
+        $form['registration_form[city]'] = 'citytest';
+        $form['registration_form[birthdate][day]'] = 2 ;
+        $form['registration_form[birthdate][month]'] = 2 ;
+        $form['registration_form[birthdate][year]'] = 2001 ;
+        $form['registration_form[agreeTerms]'] = 1 ;
+        
+        $crawler= $client->submit($form);
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler = $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('user_index', $client->getRequest()->get('_route'));
+
+        $em = self::$container->get('doctrine');
+        $user = $em->getRepository(User::class)->findOneBy([], ['id' => 'DESC']);
+        $this->assertNotEquals($user->getPassword(), '');
+        $this->assertEquals($user->getEmail(), $email);
+    }
+
+    
+
+    public function testMatchDelete()
+    {
+        $client = static::createClient();  
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('test@test.com');
+        $testUserId = $testUser->getId() ;
+        $client->loginUser($testUser);
+
+        $em = self::$container->get('doctrine');
+        
+        $crawler = $client->request('GET', '/users/pseudotest/edit');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Supprimer mon compte')->form();
+
+        $crawler= $client->submit($form);
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        
+        $em = self::$container->get('doctrine');
+        $deletedUser = $em->getRepository(User::class)->find($testUserId);
+       
+        $this->assertNull($deletedUser);
+    }
+
+}
